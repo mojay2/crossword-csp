f06c7b9a718e60650be38157310740ff619cac53
jay <jayambal50@gmail.com>
2023-03-06 04:10:59 +0800

    Automated Dictionary cycles

--- src/crosswordv2/CrosswordV2.java
+++ src/crosswordv2/CrosswordV2.java
@@ -6,9 +6,9 @@ public class CrosswordV2 {
 
     static Scanner cin = new Scanner(System.in);
 
-    //Enclose domain values in double quotes
-    public static ArrayList<String> convert(String dict){
-        ArrayList<String> dictionary = new ArrayList<>();
+    //Enclose domain values in double quotes    
+    public static TreeSet<String> convert(String dict){
+        TreeSet<String> dictionary = new TreeSet<>();
         String[] dictArr = dict.split(",");
         for(int i = 0; i<dictArr.length; i++){
             dictArr[i] = "\""+dictArr[i].trim()+"\"";
@@ -45,40 +45,78 @@ public class CrosswordV2 {
         return result;
     }
 
-    //Removes the double quotes of the arraylist
-    public static TreeSet<String> revert(TreeSet<String> result){
-        TreeSet<String> reverted = new TreeSet<>();
-        for (String x : result) {
-            String revert = x.substring(1, x.length()-1);
-            reverted.add(revert);
-        }     
-        return reverted;
+    //Cycles through each pair of variables and checks each arc contingency
+    public static HashMap<String, ArrayList> cycle(HashMap<String, ArrayList> compiled){
+        System.out.println("Cycle start:");
+            for(String key : compiled.keySet()){
+            ArrayList<String> final_result = new ArrayList();
+            if(key.startsWith("A")){
+                for(int i = 1; i <= 3; i++) {
+                    String Y = "";
+                    if(i == 1)
+                        Y = "D1";
+                    if(i == 2)
+                        Y = "D2";
+                    if(i == 3)
+                        Y = "D3";
+                    
+                    TreeSet<Character> V1_keys = retrieveKeys(compiled.get(key), i);
+                    System.out.println(key + " keys at position "+i + " : "+ V1_keys);
+                    for(String V2 : compiled.keySet()){
+                        if(V2.startsWith(Y)){
+                            int pos = Integer.valueOf(key.substring(key.length() - 1));
+                            System.out.println("ARC " +key+ " and "+V2);
+                            System.out.println("x: "+ i);
+                            System.out.println("y: "+ pos);
+                            TreeSet<String> result_tree = AC3(compiled.get(V2),V1_keys,pos);
+                            final_result = new ArrayList (result_tree);
+                            compiled.replace(V2, final_result);;
+                            System.out.println("Reduced domain:" + final_result);
+                            System.out.println("Reduced domain size: " + final_result.size()+ "\n");
+                        }
+                    }
+                }
+                compiled.replace("A1", compiled.get("D1"));                
+                compiled.replace("A2", compiled.get("D2"));
+                compiled.replace("A3", compiled.get("D3"));
+                System.out.println("");
+            }
+        }
+        return compiled;
     }
-
+   
 
     public static void main(String[] args) {
-        while(true){
-            //Enter domain values separated by commas, e.g: add, ado, lee, bee, art, arc
-            System.out.println("\nEnter domain values of Var 1:");
-            String StringX = cin.nextLine();
-            ArrayList<String> X = convert(StringX);
-            System.out.println("Enter domain values of Var 2:");
-            String StringY = cin.nextLine();
-            ArrayList<String> Y = convert(StringY);
-            System.out.println("\n***CONSTRAINT: For every Xth position of Variable 1, MUST have SOME Variable 2 with the same Yth Letter ***");
-            System.out.println("Enter X:");
-            int posX = cin.nextInt();
-            System.out.println("Enter Y:");
-            int posY = cin.nextInt();
 
-            TreeSet<Character> keys = retrieveKeys(X, posX);        
-            TreeSet<String> result = AC3(Y, keys, posY );        
-            TreeSet<String> resultFinal = revert(result);
+        String dict_string = "add, ado, age, ago, aid, ail, aim, air, and, any, "
+            + "ape, apt, arc, are, ark, arm, art, ash, ask, auk, awe, awl, aye, "
+            + "bad, bag, ban, bat, bee, boa, ear, eel, eft, far, fat, fit, lee, "
+            + "oaf, rat, tar, tie";
 
-            System.out.println("Keys of Variable 1:" + keys);        
-            System.out.println("Reduced Domain of Variable 2: "+ resultFinal);        
-            System.out.println("Size of new Domain: " + result.size());
-            cin.nextLine();
+        TreeSet<String> dict_set = convert(dict_string);
+
+        ArrayList<String> A1 = new ArrayList<String>( dict_set );
+        ArrayList<String> A2 = A1;
+        ArrayList<String> A3 = A1;
+        ArrayList<String> D1 = A1;
+        ArrayList<String> D2 = A1;
+        ArrayList<String> D3 = A1;
+
+        HashMap<String, ArrayList> compiled = new HashMap<>();
+        compiled.put("A1", A1);          
+        compiled.put("A2", A2); 
+        compiled.put("A3", A3);        
+        compiled.put("D1", D1);        
+        compiled.put("D2", D2);        
+        compiled.put("D3", D3);        
+       
+        //Cycle through dictionary 2 times, 1 cycle is not enough, 3 becomes redundant
+        compiled = cycle(compiled);
+        compiled = cycle(compiled);
+
+        System.out.println("");
+        for(String key : compiled.keySet()){
+            System.out.println(key + ":" + compiled.get(key));
         }
     }
     
